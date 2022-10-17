3a4a72627cdaeb0c655edb45b909490a5ad06679
Sam Stahl <70279507+Stormplayer5@users.noreply.github.com>
2022-10-17 15:00:15 -0600

    Add files via upload

---
+++ Useless_Box.ino
@@ -0,0 +1,12 @@
+/* Code for useless box
+ *  First iteration on 10/17/2022
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
